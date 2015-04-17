656f489c2373c576f14e5a451515fd3e595dff7c
SondreB <pixelworld@hotmail.com>
2015-04-17 14:46:50 +0200

    Fix for Azure

--- server/app.js
+++ server/app.js
@@ -6,29 +6,20 @@ var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
 //var bodyParser = require('body-parser');
 
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '');
+//app.set('view engine', '');
 //app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-/*
-app.get('/', function(req, res) {
-    res.render('index.html');
-});*/
-
 
 //app.use('/', routes);
 //app.use('/users', users);
@@ -76,6 +67,5 @@ app.use(function(err, req, res, next) {
   */
   
 });
-
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -4,28 +4,25 @@
  * Module dependencies.
  */
 
+var express = require('express');
 var app = require('./app');
 var debug = require('debug')('Pandect:server');
 var http = require('http');
 
-
 /**
  * Get port from environment and store in Express.
  */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-
 var server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
