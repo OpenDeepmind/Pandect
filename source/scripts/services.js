693b62abb93a10101c655ab02eb0690714e77cbc
SondreB <pixelworld@hotmail.com>
2015-05-31 19:50:29 +0200

    Some uncommited changed

--- source/scripts/services.js
+++ source/scripts/services.js
@@ -12,6 +12,45 @@ var angular = require('angular');
 
 	var services = angular.module('pandect.services', []);
 	
+	services.service('data', ['$rootScope', function ($rootScope) {
 	
+		var getDashboards = function () {
+			
+			var dashboards = [];
+			
+			var list = [];
+			
+			for (var i = 0; i < 10; i++) {
+				list.push({
+
+					ui: {
+						'span': 2,
+						'col': 2
+					},
+
+					'title': 'Hello World!'
+				})
+			}
+			
+			var dashboard = {
+				'title': 'Pings',
+				'icon': 'yellow',
+				'columns': 6,
+				'alerts': 4,
+				'items': list
+			};
+			
+			dashboards.push(dashboard);	
+			
+			return dashboards;		
+		};
+		
+		return {
+			
+			getDashboards: getDashboards
+			
+		};
+		
+	}]);
 	
 })();
